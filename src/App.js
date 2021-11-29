cd17ff966824171ad8b7687364c7a9f4c904e1ec
Vincent Vijn <vincent@vijn.me>
2021-11-29 10:39:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello CMD!!!!! 
         </p>
         <a
           className="App-link"
